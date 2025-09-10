202c90a9575cdc0d2ef0c5bfb192f2fdb5ee2fa0
PaolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-10 19:30:51 -0300

    Diferença de variáveis de Portugol para JS

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Aula de Javascript
-
+# Aula de JavaScript
---
+++ variaveis.js
@@ -0,0 +1,37 @@
+// Portugol para JavaScript
+// Declaração de variáveis
+
+/*
+Em portugol:
+cadeia nome = "Paola"
+cadeia sobrenome = "Borchardt"
+inteiro idade = 17 
+real altura = 1.65
+logico ehHumano = verdadeiro
+*/
+
+let nome 
+let sobrenome
+let idade 
+let altura
+let ehHumano 
+
+nome = "Paola"
+sobrenome = "Borchardt"
+idade = 17
+altura = 1.65
+ehHumano = true
+
+/*
+escreva("Nome: ", nome)
+escreva("Sobrenome: ", sobrenome)
+escreva("Idade: ", idade)
+escreva("Altura: ", altura)
+escreva("É humano? ", ehHumano)
+*/ 
+
+console.log("Nome:", nome)
+console.log("Sobrenome:", sobrenome)
+console.log("Idade:", idade)
+console.log("Altura:", altura)
+console.log("É humano?", ehHumano)
