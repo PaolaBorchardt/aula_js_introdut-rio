f1f43345165ebeaf2f528907d527b0f0c71999d4
Paola Borchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-10 23:54:05 +0000

    Adiciona estrutura condicional para aprovação e reprovacão de notas

---
+++ condicionais.js
@@ -0,0 +1,17 @@
+/*
+se (nota >= 7.0) {
+    escreva("Aprovado")
+} senao {
+    escreva("Reprovado")
+
+}*/
+
+let nota = 7
+
+if (nota >= 7 && nota <= 10) {
+  console.log("Aprovado");
+} else {
+  console.log("Reprovado");
+}
+
+console.log(typeof nota)
