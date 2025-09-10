9237d3ba0403a92c3f6c9d03e96a65a87c0dacb4
Paola Borchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-10 23:43:15 +0000

    Adiciona arquivo de exercício prático com informações pessoais

---
+++ exercicio-pratico-um.js
@@ -0,0 +1,10 @@
+//att 01
+
+let meuNome = "Paola";
+let minhaIdade = 17;
+let minhaCidade = "Pomerode - SC";
+let gostaDeProgramar = true;
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log("Gosta de programar: ",gostaDeProgramar);
