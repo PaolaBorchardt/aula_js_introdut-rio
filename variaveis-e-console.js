981eb7c324239848dea28bb1b66f681f85524412
PaolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-11 19:08:41 -0300

    Adiciona código inicial com informações pessoais e exibição em console

---
+++ variaveis-e-console.js
@@ -0,0 +1,7 @@
+// Complete o código:
+let meuNome = "Paola";
+let minhaIdade = 17;
+let minhaCidade = "Pomerode-SC";
+
+// Exiba as informações usando template literals
+console.log(`Meu nome é ${meuNome}, tenho ${minhaIdade} anos e moro em ${minhaCidade}.`);
