cbf3ff380a254de977c62553ba92dc5442de8f9a
PaolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-15 20:27:58 -0300

    Exercicio tabuada 1.2

---
+++ exercicio-tabuada-simples.js
@@ -0,0 +1,9 @@
+let numero = 7
+console.log(`Tabuada do ${numero}:`)
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i
+    console.log(`${numero} x ${i} = ${resultado}`)
+}
+
+//Fim do código
