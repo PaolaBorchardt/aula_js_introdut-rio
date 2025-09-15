7fab01217fcf85a69c0d0892b55edec275a57e70
PaolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-15 20:49:29 -0300

    Atualiza script vinculado no HTML e adiciona funções de cálculo em funcao.js

---
+++ funcao.js
@@ -0,0 +1,31 @@
+/*
+função em Portugol
+
+função real calculadoraMedia(real n1, real n2) {
+    retorna (n1 + n2) / 2
+}
+*/
+function calculadoraMedia(n1, n2) {
+    return (n1 + n2) / 2;
+}
+
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+function sub(n1, n2) {
+    return n1 - n2;
+}
+
+ function mult(n1, n2) {
+    return n1 * n2;
+}
+
+function div(n1, n2) {
+    return n1 / n2;
+}
+
+
+
+let media = calculadoraMedia(7, 8);
+console.log(`A média é: ${media}`);
