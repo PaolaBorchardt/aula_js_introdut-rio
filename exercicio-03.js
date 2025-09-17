589390fa5cfb20bc7652dea00cea3b2280997d07
PaolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-17 18:10:49 -0300

    exercicio 1.3

--- exercicio-03.js
+++ exercicio-03.js
@@ -5,3 +5,17 @@ for (let i = 1; i <= 10; i++) {
 }
 
 console.log(`A soma dos números de 1 a 10 é: ${soma}`);
+
+// Complete a função que verifica se um número é par ou ímpar
+
+function verificarParImpar(numero) {
+    if (numero % 2 === 0) {
+        return "par";
+    } else {
+        return "ímpar";
+    }
+}
+
+// Teste a função
+console.log(verificarParImpar(7));  // deve retornar "par"
+console.log(verificarParImpar(6));  // deve retornar "ímpar"
