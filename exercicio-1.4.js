f3ffb7a7fa312b4dd5d8bcdc6ffbfd1965e48dec
PaolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-17 18:15:23 -0300

    Adiciona loops para contagem de 1 a 10 e tabuada do 5

---
+++ exercicio-1.4.js
@@ -0,0 +1,12 @@
+// Complete o loop para contar de 1 a 10
+for (let i = 1; i <= 10; i++) {
+    let resultado = i;  
+    console.log("Número:", resultado);
+}
+
+// Complete o loop para tabuada do 5
+let numero = 5;
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`);
+}
