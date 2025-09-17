b1aca9c63f6646820984323dc4b1cacd9cc0cd79
PaolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-17 19:17:41 -0300

    Variaveis let, var

---
+++ diferenca.js
@@ -0,0 +1,13 @@
+//ECMAS script6
+var teste = "Olá, mundo!";
+console.log(teste);
+
+//VAR sintaxe antiga, que se aplica a todo o código
+
+{
+    let teste2 = "Olá, mundo! 2";
+    //define com let para algo dentro das chaves - bloco específico
+    console.log(teste2);
+}
+
+console.log(teste2);
