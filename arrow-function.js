3e7e6e940e5844704d7b512528829cefbf0cea81
paolaBorchardt <paola_borchardt@estudante.sesisenai.org.br>
2025-09-18 19:31:00 -0300

    Adiciona função calculadora com operações básicas e exemplos de uso

--- arrow-function.js
+++ arrow-function.js
@@ -35,3 +35,43 @@ console.log("Arrow: " + ehParArrow(4)); // true
 const subtracaoArrow = (a, b) => a - b;
 const multiplicacaoArrow = (a, b) => a * b;
 const divisaoArrow = (a, b) => a / b;*/
+
+
+//Tabuada multiplicação - Função tradicional
+/*function tabuada(numero) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${numero} x ${i} = ${numero * i}`);
+    }
+}
+    resultado = tabuada(6);
+
+    console.log("Função tradicional: " + resultado); //puxa com console.log(tabuada(6))
+
+//Tabuada multiplicação - Função Arrow function
+const tabuada = numero => {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${numero} x ${i} = ${numero * i}`);
+    }
+}
+    resultado = tabuada(6);
+
+    console.log("Função arrow: " + resultado);
+
+//puxa com console.log(tabuada(6)) */
+
+
+
+console.log("-----Calculadora------");
+
+function calculador (n1, n2, operacao) {
+    switch (operacao) {
+        case '+': return n1 + n2;
+        case '-': return n1 - n2;
+        case '*': return n1 * n2;
+        case '/': 
+            return n2 !== 0 ? n1 / n2 : "Erro: Divisão por zero";
+        default:
+            return "Operação inválida";
+    }
+}
+console.log(calculador(1, 3, '+')); // 15
